1d8cfb44669651828da6459fb6c24209a33ab6dc
Anatoli Marian <bmwm5f15.tm@gmail.com>
2024-02-18 12:36:35 +0200

    Python_Trening/task27-34

---
+++ Python_Training/task 28.py
@@ -0,0 +1,10 @@
+#Измените программу из задачи 027 так, чтобы она 
+#выводила результат с точностью до двух знаков 
+#в дробной части.
+
+import math
+
+num = float(input('Number float: '))
+result = num * 2
+rezultat = math.sqrt(result)
+print(rezultat)
