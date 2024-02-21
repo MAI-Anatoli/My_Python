1ec0f24dda634098a5d3fa4057c807d954e41eea
Anatoli Marian <bmwm5f15.tm@gmail.com>
2024-02-21 20:28:58 +0200

    Python_Trening/task48-49

---
+++ Python_Training/task48.py
@@ -0,0 +1,19 @@
+# Предложите пользователю ввести имя человека, которого
+# пользователь хочет пригласить на вечеринку. После этого вы-
+# ведите сообщение «[имя] has been invited» и увеличьте счет-
+# чик на 1. Спросите, хочет ли пользователь пригласить кого-то
+# еще. Продолжайте запрашивать имена, пока пользователь не
+# ответит отрицательно, и выведите количество приглашенных.
+
+guest_name = input('Enter guest name: ')
+count = 1
+print(guest_name,'has been invited')
+guests = input('would you like to invite someone else? y/n: ')
+while guests == 'y':
+    guest_name = input('Enter guest name: ')
+    print(guest_name,'has been invited' )
+    guests = input('would you like to invite someone else? y/n: ')
+    count += 1
+if guests == 'n':
+        print(count)
+      
