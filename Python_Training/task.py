8fe31bcef7bdfc8484660c3548021460f6652ee0
MAI-Anatoli <67876357+MAI-Anatoli@users.noreply.github.com>
2024-02-21 13:26:49 +0000

    task50

--- Python_Training/task.py
+++ Python_Training/task.py
@@ -1,17 +1,12 @@
-comp_num = 50
-user_num = int(input('Enter a num: '))
-count = 1
 
-while user_num < comp_num:
-   print('Try entering a higher number =>')       
-   user_num = int(input('Enter a num =>: ')) 
-   count += 1
-
-if user_num > comp_num:
-      print('Try entering a lower number <=')
-      user_num = int(input('Enter a num <=: '))
-      count += 1
-  
-if user_num == comp_num:
-  print(f'Well done, you took, {count} attempts') 
-
+user = int(input('Enter a number 10-20 : '))
+while user < 10:
+    print('Too low')
+    user = int(input('Enter a number 10-20 : '))
+    
+while user > 20:
+    print('Too high')
+    user = int(input('Enter a number 10-20 : ')) 
+    
+if user >= 10 and user <= 20:
+    print('Thank you')
